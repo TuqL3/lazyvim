44c7b1490c7240de93937e9ccebdadc192f0c7a5
tunglv <tunglv@avadagroup.com>
2026-05-04 09:53:08 +0700

    update minimap

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-mini/mini.map",
     version = false,
-    event = "VeryLazy",
+    event = { "BufReadPost", "BufNewFile" },
     keys = {
       { "<leader>um", function() require("mini.map").toggle() end, desc = "Minimap: toggle" },
       { "<leader>uM", function() require("mini.map").toggle_focus() end, desc = "Minimap: toggle focus" },
@@ -21,17 +21,23 @@ return {
           map.gen_integration.gitsigns(),
         },
         symbols = {
-          encode = map.gen_encode_symbols.dot("4x2"),
-          scroll_line = "█",
-          scroll_view = "┃",
+          encode = map.gen_encode_symbols.block("3x2"),
+          scroll_line = "▌",
+          scroll_view = "│",
         },
         window = {
           side = "right",
-          width = 12,
-          winblend = 25,
+          width = 10,
+          winblend = 15,
           show_integration_count = false,
+          zindex = 10,
         },
       })
+
+      vim.api.nvim_set_hl(0, "MiniMapNormal",       { link = "NormalFloat" })
+      vim.api.nvim_set_hl(0, "MiniMapSymbolView",   { fg = "#7aa2f7", bg = "NONE" })
+      vim.api.nvim_set_hl(0, "MiniMapSymbolLine",   { fg = "#bb9af7", bg = "NONE", bold = true })
+      vim.api.nvim_set_hl(0, "MiniMapSymbolCount",  { fg = "#565f89", bg = "NONE" })
 
       vim.api.nvim_create_autocmd("FileType", {
         pattern = {
@@ -46,9 +52,13 @@ return {
         callback = function()
           if vim.b.minimap_disable then
             map.close()
+          else
+            map.open()
           end
         end,
       })
+
+      map.open()
     end,
   },
 }
